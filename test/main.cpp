6cf6bfec5b42b1f659997388ba059150d3e377cc
orange <475395062@qq.com>
2023-04-30 01:46:08 +0800

    add checkFloat function

--- test/main.cpp
+++ test/main.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
+#include <iomanip>
+
 #include <cmath>
 #include "orangeMath.h"
+
+void checkFloat(float mine,float cmath)
+{
+    std::cout<<std::setprecision(12)<<std::showpos
+             <<"OMath:"<<mine<<std::endl
+             <<"CMath:"<<cmath<<std::endl
+             <<"Error:"<<mine-cmath<<' '<<(mine-cmath)/cmath*100<<'%'<<std::endl;
+}
 
 int main(int argc,char* argv[])
 {
     std::cout<<"--- Hello orangeMath ---"<<std::endl;
-    
+    checkFloat(_exp(2),exp(2));
     return 0;
 }
 
