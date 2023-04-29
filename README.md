0ce5231b7037c3ec14d01fc2c2b9fed191ee5728
orange <475395062@qq.com>
2023-04-29 16:26:26 +0800

    new file:   .gitignore 	new file:   README.md 	new file:   include/orangeMath.h 	new file:   src/CMakeLists.txt 	new file:   src/orangeMath.c 	new file:   test/CMakeLists.txt 	new file:   test/main.cpp

---
+++ README.md
@@ -0,0 +1,1 @@
+### A simple pratice of math library
---
+++ include/orangeMath.h
@@ -0,0 +1,18 @@
+#ifndef __ORANGEMATH_H
+#define __ORANGEMATH_H
+
+#ifdef __cplusplus
+extern "C"{
+#endif //__cplusplus
+
+
+
+
+
+
+
+#ifdef __cplusplus
+}
+#endif //__cplusplus
+
+#endif //__ORANGEMATH_H
