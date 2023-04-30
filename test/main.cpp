fa4b5c4e706b024208ba8e081c25da64ee039a5f
orange <475395062@qq.com>
2023-05-01 00:23:47 +0800

    modify precise goal

--- test/main.cpp
+++ test/main.cpp
@@ -6,34 +6,40 @@
 
 void checkFloat(float mine,float cmath)
 {
+    float error = (mine-cmath)/cmath;
+    if (error<0)error=-error;
+    
+    if (error>0.000001)std::cout<<"\033[0;31m";
+    else std::cout<<"\033[0;32m";
+    
     std::cout<<std::setprecision(12)<<std::showpos
-             <<"OMath:"<<mine<<std::endl
-             <<"CMath:"<<cmath<<std::endl
-             <<"Error:"<<mine-cmath<<' '<<(mine-cmath)/cmath*100<<'%'<<std::endl;
+             <<"OMath:"<<mine//<<std::endl
+             <<"\tCMath:"<<cmath//<<std::endl
+             <<"\tError:"<<error<<"\033[0m"<<std::endl;
 }
 
 
 void checkAsin(float x)
 {
-    std::cout<<"asin("<<x<<"):"<<std::endl;
+    std::cout<<"asin("<<x<<"): ";//<<std::endl;
     checkFloat(_asin(x),asin(x));
 }
 
 void checkExp(float x)
 {
-    std::cout<<"exp("<<x<<"):"<<std::endl;
+    std::cout<<"exp("<<x<<"): ";//<<std::endl;
     checkFloat(_exp(x),exp(x));
 }
 
 void checkLog(float x)
 {
-    std::cout<<"log("<<x<<"):"<<std::endl;
+    std::cout<<"log("<<x<<"): ";//<<std::endl;
     checkFloat(_log(x),log(x));
 }
 
 int main(int argc,char* argv[])
 {
-    std::cout<<"\033[0;31;42m--- Hello orangeMath ---\033[0;32m"<<std::endl;
+    std::cout<<"\033[0;32m--- Hello orangeMath ---\033[0m"<<std::endl;
     checkExp(20.0);
     checkExp(1.0);
     checkExp(-1.0);
